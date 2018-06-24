de8c4699687c52d1f2bcf50ae28546448bddd0e5
Dimitar Kokotanekov <acid901@mail.bg>
2018-06-24 17:24:06 +0300

    CRU funtionality :D

---
+++ inc/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PHP Blog</title>
+		<link rel="stylesheet" href="https://bootswatch.com/4/sketchy/bootstrap.min.css">
+	</head>
+	<body>
+	<?php include('navbar.php') ?>
